eaaec1ff09c347438f1c15aa0b2845f5e28f8ef8
pavlo <pavlohvozdetskyi@protonmail.com>
2024-08-21 03:05:20 +0300

    Update Two

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,13 +1,16 @@
-import cloudinary from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import { CLOUDINARY } from '../constants/index.js';
-import fs from 'node:fs/promises';
+import * as fs from 'node:fs/promises';
 import env from './env.js';
 
-cloudinary.v2.config({
-  secure: true,
-  cloud_name: env(CLOUDINARY.CLOUD_NAME),
-  api_key: env(CLOUDINARY.API_KEY),
-  api_secret: env(CLOUDINARY.API_SECRET),
+const cloud_name = env(CLOUDINARY.CLOUD_NAME);
+const api_key = env(CLOUDINARY.API_KEY);
+const api_secret = env(CLOUDINARY.API_SECRET);
+
+cloudinary.config({
+  cloud_name,
+  api_key,
+  api_secret,
 });
 
 export const saveFileToCloudinary = async (file) => {
